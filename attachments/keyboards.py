d25af0e5a6966fb4d144d369648fe8399d4a1ffd
ilsaf <justsairex@gmail.com>
2022-12-18 00:03:44 +0300

    moved on aiogram3

--- attachments/keyboards.py
+++ attachments/keyboards.py
@@ -1,27 +1,22 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-start = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2, one_time_keyboard=True)
-start1 = KeyboardButton('Регистрация')
-start.add(start1)
-start2 = KeyboardButton('Поиск ментора')
-start3 = KeyboardButton('Достигатор')
-start4 = KeyboardButton('Кто мы?')
-start5 = KeyboardButton('Что бот делает?')
-start.add(start2, start3, start4, start5)
+start_titles = ['Регистрация', 'Поиск ментора', 'Достигатор', 'Кто мы?', 'Что бот делает?']
+start = ReplyKeyboardMarkup(
+    keyboard=[[KeyboardButton(text=start_titles[0])],
+              [KeyboardButton(text=start_titles[1]), KeyboardButton(text=start_titles[2])],
+              [KeyboardButton(text=start_titles[3]), KeyboardButton(text=start_titles[4])], ],
+    resize_keyboard=True)
 
-search_mentor1 = ReplyKeyboardMarkup(resize_keyboard=True, row_width=1, one_time_keyboard=True)
-search_mentor_kb1 = KeyboardButton('Все понятно!')
-search_mentor_kb2 = KeyboardButton('Отмена')
-search_mentor1.add(search_mentor_kb1, search_mentor_kb2)
-
-search_mentor2 = ReplyKeyboardMarkup(resize_keyboard=True, row_width=1, one_time_keyboard=True)
-search_mentor_kb1 = KeyboardButton('Ага, всё понятно!')
-search_mentor_kb2 = KeyboardButton('Не-а, объясните ещё раз')
-search_mentor_kb3 = KeyboardButton('Отмена')
-search_mentor2.add(search_mentor_kb1, search_mentor_kb2, search_mentor_kb3)
-
-search_mentor3 = ReplyKeyboardMarkup(resize_keyboard=True, row_width=1, one_time_keyboard=True)
-search_mentor_kb1 = KeyboardButton('Интересно, сейчас изучу')
-search_mentor_kb2 = KeyboardButton('Сделаю это позже')
-search_mentor_kb3 = KeyboardButton('Отмена')
-search_mentor3.add(search_mentor_kb1, search_mentor_kb2, search_mentor_kb3)
+search_mentor_titles = ['Отмена', 'Все понятно!', 'Ага, всё понятно!', 'Не-а, объясните ещё раз',
+                        'Интересно, сейчас изучу', 'Сделаю это позже']
+search_mentor1 = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text=search_mentor_titles[1])],
+                                               [KeyboardButton(text=search_mentor_titles[0])], ],
+                                     resize_keyboard=True)
+search_mentor2 = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text=search_mentor_titles[2])],
+                                               [KeyboardButton(text=search_mentor_titles[3])],
+                                               [KeyboardButton(text=search_mentor_titles[0])], ],
+                                     resize_keyboard=True)
+search_mentor3 = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text=search_mentor_titles[4])],
+                                               [KeyboardButton(text=search_mentor_titles[5])],
+                                               [KeyboardButton(text=search_mentor_titles[0])], ],
+                                     resize_keyboard=True)
